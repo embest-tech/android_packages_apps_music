d9f02672c9f3b5f780bcbc3cea2ceab6069cee95
Hai-ting.Yin <Hai-ting.Yin@freescale.net>
2009-08-14 13:33:00 +0800

    ENGR00115340 [LMX51BBG2.5_Android] Add FSL MP3 and AAC parser node and parser component Extend the capability of freescale paser node to support Mp3 and AAC-LC/AAC-plus decoder support Signed-Off-By: Hai-ting.Yin@freescale.com

--- src/com/android/music/MediaPlaybackActivity.java
+++ src/com/android/music/MediaPlaybackActivity.java
@@ -372,6 +372,7 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
             long now = SystemClock.elapsedRealtime();
             if ((now - mLastSeekEventTime) > 250) {
                 mLastSeekEventTime = now;
+			    updateTrackInfo();
                 mPosOverride = mDuration * progress / 1000;
                 try {
                     mService.seek(mPosOverride);
@@ -959,6 +960,7 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                 } else {
                     mService.play();
                 }
+				updateTrackInfo();
                 refreshNow();
                 setPauseButtonImage();
             }
@@ -1171,7 +1173,7 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
         try {
             long pos = mPosOverride < 0 ? mService.position() : mPosOverride;
             long remaining = 1000 - (pos % 1000);
-            if ((pos >= 0) && (mDuration > 0)) {
+            if ((pos >= 0) && (mService.duration() > 0)) {
                 mCurrentTime.setText(MusicUtils.makeTimeString(this, pos / 1000));
                 
                 if (mService.isPlaying()) {
@@ -1206,6 +1208,7 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                     break;
 
                 case REFRESH:
+					updateTrackInfo();
                     long next = refreshNow();
                     queueNextRefresh(next);
                     break;
